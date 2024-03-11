16c82703b0c6ec6c91db902edebf586ca1615933
FINIKKKK <digitallkrash@gmail.com>
2024-03-11 10:56:59 +0500

    name and description change in pubscep.yallm

--- test/lib/constants/blocks.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-/*
-  Block Global Styles ----------------
- */
-class BlockStyles {
-  static const EdgeInsets pad = EdgeInsets.symmetric(vertical: 16, horizontal: 27);
-}
