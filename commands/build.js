faad00111c91d8d65932b0252676f1db64ca34c1
FINIK <digitallkrash@gmail.com>
2024-03-12 23:26:06 +0500

    new_structure

---
+++ commands/build.js
@@ -0,0 +1,54 @@
+const { exec } = require('child_process');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function (program) {
+    program
+        .command('build [destination]')
+        .description('Клонирует репозиторий. Если указан аргумент ".", клонирование происходит в текущую директорию.')
+        .action(async (destination) => {
+            const inquirer = await import('inquirer');
+
+            const answers = await inquirer.default.prompt([
+                {
+                    type: 'input',
+                    name: 'projectName',
+                    message: 'Введите имя проекта:',
+                    default: () => destination ? path.basename(destination) : "my-new-project"
+                }
+            ]);
+
+            console.log(`Клонирование репозитория`);
+            const repoUrl = "https://github.com/FINIKKKK/flutter-template";
+            const projectName = path.basename(repoUrl, '.git');
+            console.log(`Имя проекта: ${answers.projectName}`);
+            console.log(`Клонирование репозитория: ${repoUrl}`);
+
+            let cloneDirectory = destination === '.' ? '.' : projectName;
+            let cloneCommand = `git clone ${repoUrl} ${cloneDirectory}`;
+
+            exec(cloneCommand, (cloneError, cloneStdout, cloneStderr) => {
+                if (cloneError) {
+                    console.error(`Ошибка при клонировании: ${cloneError.message}`);
+                    return;
+                }
+                if (cloneStderr) {
+                    console.log(`Информация о клонировании: ${cloneStderr}`);
+                }
+
+                console.log(cloneStdout || 'Репозиторий успешно клонирован.');
+
+                const gitDirPath = cloneDirectory === '.' ? path.join('.', '.git') : path.join(process.cwd(), cloneDirectory, '.git');
+                console.log(`Удаление папки .git`);
+                fs.rm(gitDirPath, { recursive: true, force: true }, (rmError) => {
+                    if (rmError) {
+                        console.error(`Ошибка при удалении папки .git: ${rmError}`);
+                        return;
+                    }
+                    console.log('Папка .git успешно удалена.');
+                });
+            });
+        });
+
+}
--- index.js
+++ index.js
@@ -1,61 +1,13 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const { exec } = require('child_process');
-
-const fs = require('fs');
-const path = require('path');
+const buildCommand = require('./commands/build');
 
 program
   .name('mycli')
   .description('CLI для клонирования репозитория с помощью git clone')
   .version('1.0.0');
 
-program
-  .command('build [destination]')
-  .description('Клонирует репозиторий. Если указан аргумент ".", клонирование происходит в текущую директорию.')
-  .action(async (destination) => {
-    const inquirer = await import('inquirer');
-
-    const answers = await inquirer.default.prompt([
-      {
-        type: 'input',
-        name: 'projectName',
-        message: 'Введите имя проекта:',
-        default: () => destination ? path.basename(destination) : "my-new-project"
-      }
-    ]);
-
-    console.log(`Клонирование репозитория`);
-    const repoUrl = "https://github.com/FINIKKKK/flutter-template";
-    const projectName = path.basename(repoUrl, '.git');
-    console.log(`Имя проекта: ${answers.projectName}`);
-    console.log(`Клонирование репозитория: ${repoUrl}`);
-
-    let cloneDirectory = destination === '.' ? '.' : projectName;
-    let cloneCommand = `git clone ${repoUrl} ${cloneDirectory}`;
-
-    exec(cloneCommand, (cloneError, cloneStdout, cloneStderr) => {
-      if (cloneError) {
-        console.error(`Ошибка при клонировании: ${cloneError.message}`);
-        return;
-      }
-      if (cloneStderr) {
-        console.log(`Информация о клонировании: ${cloneStderr}`);
-      }
-
-      console.log(cloneStdout || 'Репозиторий успешно клонирован.');
-
-      const gitDirPath = cloneDirectory === '.' ? path.join('.', '.git') : path.join(process.cwd(), cloneDirectory, '.git');
-      console.log(`Удаление папки .git`);
-      fs.rm(gitDirPath, { recursive: true, force: true }, (rmError) => {
-        if (rmError) {
-          console.error(`Ошибка при удалении папки .git: ${rmError}`);
-          return;
-        }
-        console.log('Папка .git успешно удалена.');
-      });
-    });
-  });
+buildCommand(program);
 
 program.parse(process.argv);
