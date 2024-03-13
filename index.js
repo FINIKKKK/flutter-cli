c495e94984f0b0dfbc791c243f9dc4139fff5e53
FINIKKKK <digitallkrash@gmail.com>
2024-03-13 10:20:13 +0500

    decsription and id

--- index.js
+++ index.js
@@ -1,61 +1,79 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-const { exec } = require('child_process');
+const {program} = require('commander');
+const {exec} = require('child_process');
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 program
-  .name('mycli')
-  .description('CLI для клонирования репозитория с помощью git clone')
-  .version('1.0.0');
+    .name('mycli')
+    .description('CLI для клонирования репозитория с помощью git clone')
+    .version('1.0.0');
 
 program
-  .command('build [destination]')
-  .description('Клонирует репозиторий. Если указан аргумент ".", клонирование происходит в текущую директорию.')
-  .action(async (destination) => {
-    const inquirer = await import('inquirer');
+    .command('build [destination]')
+    .description('Клонирует репозиторий. Если указан аргумент ".", клонирование происходит в текущую директорию.')
+    .action(async (destination) => {
+        const inquirer = await import('inquirer');
 
-    const answers = await inquirer.default.prompt([
-      {
-        type: 'input',
-        name: 'projectName',
-        message: 'Введите имя проекта:',
-        default: () => destination ? path.basename(destination) : "my-new-project"
-      }
-    ]);
+        const answers = await inquirer.default.prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: 'Введите имя проекта:',
+                default: () => destination ? path.basename(destination) : "my-new-project"
+            },
+            {
+                type: 'input',
+                name: 'projectDescription',
+                message: 'Введите описание проекта:',
+                default: () => destination ? path.basename(destination) : "my-new-project"
+            }
+        ]);
 
-    console.log(`Клонирование репозитория`);
-    const repoUrl = "https://github.com/FINIKKKK/flutter-template";
-    const projectName = path.basename(repoUrl, '.git');
-    console.log(`Имя проекта: ${answers.projectName}`);
-    console.log(`Клонирование репозитория: ${repoUrl}`);
+        console.log(`Клонирование репозитория`);
+        const repoUrl = "https://github.com/FINIKKKK/flutter-template";
+        const projectName = path.basename(repoUrl, '.git');
+        const nameProject = answers.projectName.toLowerCase().replace(/\s+/g, '_');
+        const id = `com.example.${nameProject}`;
 
-    let cloneDirectory = destination === '.' ? '.' : projectName;
-    let cloneCommand = `git clone ${repoUrl} ${cloneDirectory}`;
+        console.log(`Имя проекта: ${nameProject}`);
+        console.log(`Id проекта: ${id}`);
+        console.log(`Клонирование репозитория: ${repoUrl}`);
 
-    exec(cloneCommand, (cloneError, cloneStdout, cloneStderr) => {
-      if (cloneError) {
-        console.error(`Ошибка при клонировании: ${cloneError.message}`);
-        return;
-      }
-      if (cloneStderr) {
-        console.log(`Информация о клонировании: ${cloneStderr}`);
-      }
+        let cloneDirectory = destination === '.' ? '.' : projectName;
+        let cloneCommand = `git clone ${repoUrl} ${cloneDirectory}`;
 
-      console.log(cloneStdout || 'Репозиторий успешно клонирован.');
+        exec(cloneCommand, async (cloneError, cloneStdout, cloneStderr) => {
+            if (cloneError) {
+                console.error(`Ошибка при клонировании: ${cloneError.message}`);
+                return;
+            }
+            if (cloneStderr) {
+                console.log(`Информация о клонировании: ${cloneStderr}`);
+            }
 
-      const gitDirPath = cloneDirectory === '.' ? path.join('.', '.git') : path.join(process.cwd(), cloneDirectory, '.git');
-      console.log(`Удаление папки .git`);
-      fs.rm(gitDirPath, { recursive: true, force: true }, (rmError) => {
-        if (rmError) {
-          console.error(`Ошибка при удалении папки .git: ${rmError}`);
-          return;
-        }
-        console.log('Папка .git успешно удалена.');
-      });
+            console.log(cloneStdout || 'Репозиторий успешно клонирован.');
+
+            console.log(`Удаление папки .git`);
+            const gitDirPath = cloneDirectory === '.' ? path.join(process.cwd(), '.git') : path.join(process.cwd(), cloneDirectory, '.git');
+            console.log(`Удаление папки .git в ${gitDirPath}...`);
+            await fs.rm(gitDirPath, {recursive: true, force: true});
+
+            console.log('Папка .git успешно удалена.');
+
+            const pubspecPath = cloneDirectory === '.' ? path.join(process.cwd(), 'pubspec.yaml') : path.join(process.cwd(), cloneDirectory, 'pubspec.yaml');
+            try {
+                let content = await fs.readFile(pubspecPath, {encoding: 'utf8'});
+                content = content.replace(/(name:\s+).*/, `$1${nameProject}`);
+                content = content.replace(/(description:\s+).*/, `$1${answers.projectDescription}`);
+                await fs.writeFile(pubspecPath, content, {encoding: 'utf8'});
+                console.log('Имя проекта в pubspec.yaml успешно обновлено.');
+            } catch (err) {
+                console.error(`Ошибка при обновлении имени проекта в pubspec.yaml: ${err}`);
+            }
+        });
     });
-  });
 
 program.parse(process.argv);
